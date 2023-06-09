05564cd49df3e6725ec420a5b93f9685f95f6fe9
Jean-Baptiste Casanave <46540647+jibc@users.noreply.github.com>
2023-06-09 18:48:55 +0200

    js fundamentals part 1

---
+++ Foundations/js-fundamental-part1.js
@@ -0,0 +1,24 @@
+let admin;
+    let name;
+    name = "John";
+    admin = name;
+    alert("admin");
+
+    let planet = "Earth";
+    let CurrentVisitor = "haskell";
+
+    const BIRTHDAY = "34.34.2034";
+
+    console.log(23+97);
+    console.log(1+2+3+4+5+6);
+    console.log((4+6+9)/77);
+
+    let a = 10;
+    console.log(a);
+    console.log(9*a);
+    let b = 7 * a;
+    console.log(b);
+
+    const MAX = 57;
+    let actual = MAX - 13;
+    percentage = actual / MAX;

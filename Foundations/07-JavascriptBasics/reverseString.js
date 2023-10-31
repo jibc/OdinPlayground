aca49a58e31ba1bc4e9a2b9548fab6a25acf9b43
Jean-Baptiste Casanave <46540647+jibc@users.noreply.github.com>
2023-10-31 09:57:39 +0100

    03-reverseString

---
+++ Foundations/07-JavascriptBasics/reverseString.js
@@ -0,0 +1,8 @@
+const reverseString = function(string) {
+  let reverseString = "";
+  reverseString = string.split("").reverse().join("");
+  return reverseString;
+};
+
+// Do not edit below this line
+module.exports = reverseString;

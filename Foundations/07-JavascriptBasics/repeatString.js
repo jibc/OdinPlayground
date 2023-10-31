7ede94595cb595a822105a45434eee3b6501c6ea
Jean-Baptiste Casanave <46540647+jibc@users.noreply.github.com>
2023-10-31 07:43:39 +0100

    02-test

---
+++ Foundations/07-JavascriptBasics/repeatString.js
@@ -0,0 +1,10 @@
+const repeatString = function(string, num) {
+  let sentence = "";
+  for (let i = 0; i < num; i++) {
+   sentence += string;
+}
+  return sentence;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
